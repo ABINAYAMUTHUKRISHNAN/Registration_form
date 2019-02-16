46484806cc66e66ea33bd6c8bd232f30dcff4c27
ABINAYA MUTHUKRISHNAN <abinayageetha02@gmail.com>
2019-02-16 16:19:17 +0530

    spa working

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Form_sam from "./Components/Form_sam";
+import Details from "./Components/Details";
+import View2 from "./Components/View2";
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <Details />;
   }
 }
 
